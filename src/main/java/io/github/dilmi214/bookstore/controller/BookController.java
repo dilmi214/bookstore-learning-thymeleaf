ab212a5cd57583204b782a0ccf5addb166a79a94
dilmi214 <dilmifernando214@gmail.com>
2024-06-14 12:19:11 +0530

    home page

---
+++ src/main/java/io/github/dilmi214/bookstore/controller/BookController.java
@@ -0,0 +1,12 @@
+package io.github.dilmi214.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
